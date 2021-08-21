3bd3e6be56c98db884f68efbf67677897ed86b2e
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-21 17:24:28 +0700

    Add file configuration to settings func

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ params = {
     "exclude": "current,minutely,daily,alerts"
 }
 
-try:
-    with open("credentials.json", "r") as file:
-        data = json.load(file)
-        params["lon"] = data["lon"]
-        params["lat"] = data["lat"]
-        params["appid"] = data["appid"]
-        params["lang"] = data["lang"]
-except (FileNotFoundError, json.decoder.JSONDecodeError):
-    params["lon"] = float(input("Enter longitude of your city: "))
-    params["lat"] = float(input("Enter latitude of your city: "))
-    params["appid"] = input("Enter APPID(from openweathermap.org): ")
-    params["lang"] = input("Enter language(ru/en): ").title()
-    with open("credentials.json", "w") as data:
-        json.dump(params, data)
+
+def settings():
+    global params
+    try:
+        with open("credentials.json", "r") as file:
+            data = json.load(file)
+            params["lon"] = data["lon"]
+            params["lat"] = data["lat"]
+            params["appid"] = data["appid"]
+            params["lang"] = data["lang"]
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        params["lon"] = float(input("Enter longitude of your city: "))
+        params["lat"] = float(input("Enter latitude of your city: "))
+        params["appid"] = input("Enter APPID(from openweathermap.org): ")
+        params["lang"] = input("Enter language(ru/en): ").title()
+        with open("credentials.json", "w") as data:
+            json.dump(params, data)
 
 
-
-
-
-
-print(params)
-
+settings()
 response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=params)
 response.raise_for_status()
 weather_data = response.json()
+
 print(weather_data)
